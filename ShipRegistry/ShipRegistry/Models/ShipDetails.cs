22bb44d9e155ecc27607a22355b3239eb7554474
Steve Logan <stevelogan.email@gmail.com>
2019-06-03 12:58:37 -0400

    API complete

---
+++ ShipRegistry/ShipRegistry/Models/ShipDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShipRegistry.Models
+{
+    public partial class ShipDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mass { get; set; }
+        public int TopSpeed { get; set; }
+        public int PowerRating { get; set; }
+    }
+}
